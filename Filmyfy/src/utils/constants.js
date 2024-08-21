ac7131bd4add5d55d0cb32b734dae4b639d43e8e
Aditi <261926@nttdata.com>
2024-08-21 23:43:27 +0530

    CREATE_YOUR_OWN_API_KEY

--- Filmyfy/src/utils/constants.js
+++ Filmyfy/src/utils/constants.js
@@ -1,4 +1,4 @@
 export const CDN_images = "https://image.tmdb.org/t/p/original/";
 
 export const movieAPI =
-  "https://api.themoviedb.org/3/movie/popular?api_key=dc339b77e79251625e6afd9674a4a476&page=";
+  "https://api.themoviedb.org/3/movie/popular?api_key={CREATE_YOUR_OWN_API_KEY}&page=";
